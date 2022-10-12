30891cd3e02ef457ee15fa9350ecaecd43329e7d
VNiten <103785222+VNiten@users.noreply.github.com>
2022-10-12 15:13:32 -0300

    Delete discountpricecalculator.py

--- discountpricecalculator.py
+++
@@ -1,6 +0,0 @@
-product = input('Write the product name: ')
-price = int(input('Write the price: '))
-discount = int(input('Enter the discount percentage: '))
-finalPrice = price - discount * price / 100
-
-print ('The price of the ' + product + ' is ' + (str(finalPrice)))
